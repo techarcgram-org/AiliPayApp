58a355dd3e616cfc7ce0eac180209ec51072fd81
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-12-01 11:37:33 +0100

    transactions data

---
+++ src/utils/data/transactions.js
@@ -0,0 +1,16 @@
+const generateMockData = () => {
+  const mockData = [];
+
+  for (let i = 1; i <= 28; i++) {
+    const day = Math.floor(Math.random() * 30) + 1; // Random day between 1 and 30
+    const month = Math.floor(Math.random() * 12) + 1; // Random month between 1 and 12
+    const year = 2023; // Fixed year for example purposes
+
+    const date = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
+    const amount = `XAF ${Math.floor(Math.random() * 250001) + 50000}`; // Random amount between 50000 and 300000
+
+    mockData.push({ date, amount });
+  }
+
+  return mockData;
+};
