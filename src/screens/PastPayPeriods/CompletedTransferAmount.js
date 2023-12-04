e6dedd59338b2066c6edaaca2ffda587ec3cfb04
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-12-04 10:05:54 +0100

    skelton loader

--- src/screens/PastPayPeriods/CompletedTransferAmount.js
+++ src/screens/PastPayPeriods/CompletedTransferAmount.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import { generatemockDataFrom } from '../../utils/data/transactionsFrom';
 import { generateMockDataTo } from '../../utils/data/transactionsTo';
-import { ScrollView } from 'react-native';
-
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder'; 
 
 export default function CompletedTransferAmount({ navigation }) {
-  const mockDataFrom = generatemockDataFrom()
-  console.log(mockDataFrom)
-  const mockDataTo = generateMockDataTo()
-  console.log(mockDataTo)
+  const [loading, setLoading] = useState(true);
+  const [mockDataFrom, setMockDataFrom] = useState([]);
+  const [mockDataTo, setMockDataTo] = useState([]);
+
+  useEffect(() => {
+    fetchMockData();
+  }, []);
+
+  const fetchMockData = () => {
+    const mockDataFrom = generatemockDataFrom();
+    const mockDataTo = generateMockDataTo();
+
+    // Simulating network delay
+    setTimeout(() => {
+      setMockDataFrom(mockDataFrom);
+      setMockDataTo(mockDataTo);
+      setLoading(false);
+    }, 2000);
+  };
 
   const renderTransferItemFrom = ({ item }) => (
     <View style={styles.row}>
@@ -37,30 +51,45 @@ export default function CompletedTransferAmount({ navigation }) {
       <Text style={{ fontSize: 16 }}>Account Number: {item.accountNumber}</Text>
     </View>
   );
+
+  const renderSkeletonLoader = () => (
+    <SkeletonPlaceholder>
+      <View style={styles.row} />
+      <View style={styles.row} />
+      <View style={styles.row} />
+    </SkeletonPlaceholder>
+  );
+
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <TouchableOpacity onPress={() => navigation.goBack()}>
         <Ionicons name="md-arrow-back" size={30} color="black" />
       </TouchableOpacity>
 
       <Text style={styles.headerText}>Completed Transfer Amount</Text>
-      <View>
+
+      {loading ? (
+        renderSkeletonLoader()
+      ) : (
         <FlatList
           data={mockDataFrom}
           renderItem={renderTransferItemFrom}
           keyExtractor={(item, index) => index.toString()}
         />
-      </View>
+      )}
 
       <Text style={styles.headerText}>Transferred To</Text>
-      <View>
+
+      {loading ? (
+        renderSkeletonLoader()
+      ) : (
         <FlatList
           data={mockDataTo}
           renderItem={renderTransferItemTo}
           keyExtractor={(item, index) => index.toString()}
         />
-      </View>
-    </ScrollView>
+      )}
+    </View>
   );
 }
 
@@ -71,7 +100,7 @@ const styles = StyleSheet.create({
     color: 'white',
     padding: 40,
     flexDirection: 'column',
-    paddingBottom:16
+    paddingBottom: 16
   },
   headerText: {
     color: '#3F5F90',
