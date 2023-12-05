cd35bf6ac527d18c1a1f4f860515dc18d009fb3e
Sheyrhon <chisheyrhon96@gmail.com>
2023-12-05 11:58:40 +0100

    added commas

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { useEffect, useCallback, useContext } from 'react';
+// import { useEffect, useCallback, useContext } from 'react';
 import Toast from 'react-native-toast-message';
 import Routes from './src/routes';
 import { StateProvider, store } from './store';
 import { getUserAccountSettings } from './src/services';
 import i18next from './src/services/i18next';
+import en from './locales/en.json';
+import fr from './locales/fr.json';
 
 export default function App() {
   return (
@@ -12,4 +14,4 @@ export default function App() {
       <Toast visibilityTime={9000} />
     </StateProvider>
   );
-}
+};
--- src/services/i18next.js
+++ src/services/i18next.js
@@ -5,13 +5,13 @@ import fr from '../locales/fr.json';
 
 export const languageResources = {
   en: { translation: en },
-  fr: { translation: fr }
+  fr: { translation: fr },
 };
 i18next.use(initReactI18next).init({
   compatibilityJSON: 'v3',
   lng: 'en',
   fallbackLng: 'en',
-  resources: languageResources
+  resources: languageResources,
 });
 
 export default i18next;
