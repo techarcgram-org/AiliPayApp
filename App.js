a1b85e3bdcf27c0a5adade72ead73de77fded948
Sheyrhon <chisheyrhon96@gmail.com>
2023-12-06 06:49:07 +0100

    added a modal to switch language

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button,Modal,StyleSheet,Dimensions } from 'react-native';
 import Toast from 'react-native-toast-message';
 import Routes from './src/routes';
 import { StateProvider, store } from './store';
@@ -9,15 +9,15 @@ import { I18nextProvider, useTranslation } from 'react-i18next';
 
 export default function App() {
   const [showLanguageSwitcher, setShowLanguageSwitcher] = useState(true);
-  const [showHideButton, setShowHideButton]= useState(true); 
+  const [showModal, setShowModal]=useState(false);
 
-  const handleHideButton = ()=>{
-    setShowHideButton(false);
-    setShowLanguageSwitcher(false);
+  const handleOpenModal =()=>{
+    setShowModal(true);
   };
-  if (!showLanguageSwitcher || !showHideButton){
-    return null;
-  }
+  const handleCloseModal =()=>{
+    setShowModal(false);
+
+  };
 
   return (
     <I18nextProvider i18n={i18n}>
@@ -25,27 +25,55 @@ export default function App() {
         <Routes />
         <Toast visibilityTime={9000} />
         
-        {/* <Button
-          title="Hide Language Switcher"
-          onPress={handleHideButton}
-        /> */}
-        <LanguageSwitcher />
+        {showModal && (
+         <Modal visible={true} animationType='slide-left' >
+          <View style={styles.modalontainer}>
+            <View style={styles.modalContent}>
+              <LanguageSwitcher onClose ={handleCloseModal}/>
+            </View>
+          </View>
+         
+         </Modal> 
+        )}
+        <Button title='Open Language Switcher'
+        onPress={handleOpenModal}/>
+       
       </StateProvider>
     </I18nextProvider>
   );
 }
 
-function LanguageSwitcher() {
+function LanguageSwitcher({onClose}) {
   const { t, i18n } = useTranslation();
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
+  const handleClose =()=>{
+    onClose();
+  }
 
   return (
     <View>
       <Button title={t('switchToEnglish')} onPress={() => changeLanguage('en')}  />
       <Button title={t('switchToFrench')} onPress={() => changeLanguage('fr')} />
+      <Button  title='close' onPress={handleClose}/>
     </View>
   );
 }
+
+const styles =StyleSheet.create({
+  modalontainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'rgba(0,0,0,0.5)',
+  },
+  modalContent:{
+    backgroundColor:'#fff',
+    width:'75%',
+    maxHeight: Dimensions.get('window').height/4,
+    padding:16,
+    borderRadius:8,
+  },
+});
