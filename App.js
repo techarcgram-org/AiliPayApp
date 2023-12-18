88ecae1a614ddf19a92116683a92576982319c67
Sheyrhon <chisheyrhon96@gmail.com>
2023-12-18 08:32:43 +0100

    took off commented code

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button,Modal,StyleSheet,Dimensions } from 'react-native';
+import { View, Text, Button, Modal, StyleSheet, Dimensions } from 'react-native';
 import Toast from 'react-native-toast-message';
 import Routes from './src/routes';
 import { StateProvider, store } from './store';
@@ -8,73 +8,18 @@ import i18n from './src/services/i18next'; // Import the i18n.js file
 import { I18nextProvider, useTranslation } from 'react-i18next';
 
 export default function App() {
- 
-  const [showModal, setShowModal]=useState(false);
 
-  const handleOpenModal =()=>{
-    setShowModal(true);
-  };
-  const handleCloseModal =()=>{
-    setShowModal(false);
 
-  };
 
   return (
     <I18nextProvider i18n={i18n}>
       <StateProvider>
         <Routes />
         <Toast visibilityTime={9000} />
-        
-        {showModal && (
-         <Modal visible={true} animationType='slide-left' >
-          <View style={styles.modalontainer}>
-            <View style={styles.modalContent}>
-              <LanguageSwitcher onClose ={handleCloseModal}/>
-            </View>
-          </View>
-         
-         </Modal> 
-        )}
-        
-        <Button title='Open Language Switcher'
-        onPress={handleOpenModal}/>
-       
+
+
       </StateProvider>
     </I18nextProvider>
   );
 }
 
-function LanguageSwitcher({onClose}) {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-  const handleClose =()=>{
-    onClose();
-  }
-
-  return (
-    <View>
-      <Button title={t('switchToEnglish')} onPress={() => changeLanguage('en')}  />
-      <Button title={t('switchToFrench')} onPress={() => changeLanguage('fr')} />
-      <Button  title='close' onPress={handleClose}/>
-    </View>
-  );
-}
-
-const styles =StyleSheet.create({
-  modalontainer:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:'rgba(0,0,0,0.5)',
-  },
-  modalContent:{
-    backgroundColor:'#fff',
-    width:'75%',
-    maxHeight: Dimensions.get('window').height/4,
-    padding:16,
-    borderRadius:8,
-  },
-});
--- src/screens/LangingScreen/LandingScreen.js
+++ src/screens/LangingScreen/LandingScreen.js
@@ -1,42 +1,111 @@
-import { StyleSheet, View } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Dimensions,
+  Button,
+  Modal
+} from "react-native";
 import Logo from "../../components/Logo";
 import CustomButton from "../../components/CustomButton";
 import DisplayBox from "../../components/DisplayBox";
-import i18next, {languageResources} from '../../services/i18next'
-import {useTranslation} from 'react-i18next'
+import i18n, { languageResources } from '../../services/i18next';
+import React, { useState } from 'react';
+
+
+import { useTranslation, I18nextProvider } from 'react-i18next'
+// import ChangeLanguage from "../../components/ChangeLanguage";
+
 
 export default function LandingScreen2({ navigation }) {
-  const {t} = useTranslation()
+  const { t } = useTranslation()
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+
+  };
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Logo />
-        <CustomButton
-          title="login"
-          backgroundColor="#3F5F90"
-          color="white"
-          onPress={() => navigation.navigate('LoginScreen')}
-        />
-      </View>
-      <View style={styles.image}></View>
-      <View style={styles.info}>
-        <DisplayBox
-          header={t('landing2.text1')}
-          body={t('landing2.text2')}
-          footer={
+    <ScrollView>
+      <I18nextProvider i18n={i18n}>
+
+
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Logo />
             <CustomButton
-              title={t('landing2.button')}
-              backgroundColor="#063B87"
+              title="login"
+              backgroundColor="#3F5F90"
               color="white"
-              onPress={() => navigation.navigate('GettingStartedEmployeeIdScreen')}
+              onPress={() => navigation.navigate('LoginScreen')}
             />
-          }
-        />
-      </View>
-    </View>
+          </View>
+          <View style={styles.image}></View>
+          <View style={styles.info}>
+            <DisplayBox
+              header={t('landing2.text1')}
+              body={t('landing2.text2')}
+              footer={
+                <CustomButton
+                  title={t('landing2.button')}
+                  backgroundColor="#063B87"
+                  color="white"
+                  onPress={() => navigation.navigate('GettingStartedEmployeeIdScreen')}
+                />
+              }
+            />
+          </View>
+        </View>
+
+
+
+        {showModal && (
+          <Modal visible={true} animationType='slide-left' >
+            <View style={styles.modalontainer}>
+              <View style={styles.modalContent}>
+                <LanguageSwitcher onClose={handleCloseModal} />
+              </View>
+            </View>
+
+          </Modal>
+        )}
+        <View style={styles.lngswitchbtn}>
+          <Button title='Open Language Switcher'
+          color={'#063B87'}
+
+            onPress={handleOpenModal} />
+        </View>
+
+      </I18nextProvider>
+
+
+
+    </ScrollView>
   );
 }
 
+function LanguageSwitcher({ onClose }) {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+  const handleClose = () => {
+    onClose();
+  }
+
+  return (
+    <View>
+      <Button title={t('switchToEnglish')} onPress={() => changeLanguage('en')} />
+      <Button title={t('switchToFrench')} onPress={() => changeLanguage('fr')} />
+      <Button title='close' onPress={handleClose} />
+    </View>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -61,4 +130,21 @@ const styles = StyleSheet.create({
   info: {
     marginTop: 30,
   },
+  modalontainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    width: '75%',
+    maxHeight: Dimensions.get('window').height / 4,
+    padding: 16,
+    borderRadius: 8,
+  },
+  lngswitchbtn:{
+    // backgroundColor:'blue',
+    // width:'80%',
+  }
 });
